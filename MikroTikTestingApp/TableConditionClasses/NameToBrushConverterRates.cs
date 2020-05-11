250e2495e525f8c11ca0297f2c7c6c23583e5c43
ullanor <pawel.nowacki95@gmail.com>
2020-05-11 19:59:25 +0200

    get tx rates quality

--- MikroTikTestingApp/TableConditionClasses/NameToBrushConverterRates.cs
+++ MikroTikTestingApp/TableConditionClasses/NameToBrushConverterRates.cs
@@ -12,18 +12,35 @@ namespace MikroTikTestingApp
     class NameToBrushConverterRates : IValueConverter
     {
         string input = string.Empty;
+        char[] testArray;
+        int r = 0;
+        string number = string.Empty;
+        float result = 0;
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             input = value as string;
-            if (input != string.Empty && input.Length > 18)
-                return Brushes.LightGreen;//Red
-            else
-                return Brushes.Red; //return DependencyProperty.UnsetValue;
+            //if (input != string.Empty && input.Length > 18)
+            //    return Brushes.LightGreen;//Red
+            //else
+            //    return Brushes.Red; //return DependencyProperty.UnsetValue;
+            if (input.Length < 3)
+                return Brushes.Red;
+            //MessageBox.Show(input.Length.ToString());
 
-            //    default:
-            //        return DependencyProperty.UnsetValue;
-            //}
+            testArray = input.ToCharArray();
+            r = Array.IndexOf(testArray, 'R');
+
+             if(r - 8 < 0)
+                return Brushes.Red;
+
+            number = input.Substring(3, r - 8);
+            float.TryParse(number.Replace('.', ','), out result);
+
+            if(result >= 50)
+                return Brushes.LightGreen;
+            else
+                return Brushes.Red;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
